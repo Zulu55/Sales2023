d38a0ddc62986b76e0d5ffa623d99d5b1216484c
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2023-02-13 16:40:36 -0500

    Set docker connection by default

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=AzureConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
 
 var app = builder.Build();
 
