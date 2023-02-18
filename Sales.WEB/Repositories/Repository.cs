be41c58375bb763a6727d70ab1ab45842c1f1dfd
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2023-02-18 12:04:06 -0500

    Fix error

--- Sales.WEB/Repositories/Repository.cs
+++ Sales.WEB/Repositories/Repository.cs
@@ -84,32 +84,5 @@ namespace Sales.WEB.Repositories
             var respuestaString = await httpResponse.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<T>(respuestaString, jsonSerializerOptions)!;
         }
-
-        public async Task<HttpResponseWrapper<object>> Delete(string url)
-        {
-            var responseHTTP = await _httpClient.DeleteAsync(url);
-            return new HttpResponseWrapper<object>(null, !responseHTTP.IsSuccessStatusCode, responseHTTP);
-        }
-
-        public async Task<HttpResponseWrapper<object>> Put<T>(string url, T model)
-        {
-            var messageJSON = JsonSerializer.Serialize(model);
-            var messageContent = new StringContent(messageJSON, Encoding.UTF8, "application/json");
-            var responseHttp = await _httpClient.PutAsync(url, messageContent);
-            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
-        }
-
-        public async  Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model)
-        {
-            var messageJSON = JsonSerializer.Serialize(model);
-            var messageContent = new StringContent(messageJSON, Encoding.UTF8, "application/json");
-            var responseHttp = await _httpClient.PutAsync(url, messageContent);
-            if (responseHttp.IsSuccessStatusCode)
-            {
-                var response = await UnserializeAnswer<TResponse>(responseHttp, _jsonDefaultOptions);
-                return new HttpResponseWrapper<TResponse>(response, false, responseHttp);
-            }
-            return new HttpResponseWrapper<TResponse>(default, !responseHttp.IsSuccessStatusCode, responseHttp);
-        }
     }
 }
