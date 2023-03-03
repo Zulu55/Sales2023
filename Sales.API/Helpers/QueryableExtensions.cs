3f3c41edec46229aef54110347d0da70d9a6f239
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2023-03-02 21:14:29 -0500

    Add pagination and filters

---
+++ Sales.API/Helpers/QueryableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Sales.Shared.DTOs;
+
+namespace Sales.API.Helpers
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO pagination)
+        {
+            return queryable
+                .Skip((pagination.Page - 1) * pagination.RecordsNumber)
+                .Take(pagination.RecordsNumber);
+        }
+    }
+}
---
+++ Sales.Shared/DTOs/PaginationDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Sales.Shared.DTOs
+{
+    public class PaginationDTO
+    {
+        public int Id { get; set; }
+
+        public int Page { get; set; } = 1;
+
+        public int RecordsNumber { get; set; } = 10;
+
+        public string? Filter { get; set; }
+    }
+}
