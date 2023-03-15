14425d143b66fd7964b385159b95eea802c38bb3
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-03-15 18:48:03 -0500

    Change password

--- Sales.API/Controllers/AccountsController.cs
+++ Sales.API/Controllers/AccountsController.cs
@@ -111,6 +111,30 @@ namespace Sales.API.Controllers
             return Ok(await _userHelper.GetUserAsync(User.Identity!.Name!));
         }
 
+        [HttpPost("changePassword")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> ChangePasswordAsync(ChangePasswordDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userHelper.GetUserAsync(User.Identity!.Name!);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _userHelper.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.FirstOrDefault()!.Description);
+            }
+
+            return NoContent();
+        }
+
         private TokenDTO BuildToken(User user)
         {
             var claims = new List<Claim>
---
+++ Sales.Shared/DTOs/ChangePasswordDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.DTOs
+{
+    public class ChangePasswordDTO
+    {
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña actual")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string CurrentPassword { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Nueva contraseña")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string NewPassword { get; set; } = null!;
+
+        [Compare("NewPassword", ErrorMessage = "La nueva contraseña y la confirmación no son iguales.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmación nueva contraseña")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Confirm { get; set; } = null!;
+    }
+}
