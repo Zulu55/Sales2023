ac96ab46507c273f4982519ebbbc38c0d344da69
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-03-16 21:30:31 -0500

    Resend confirmation email

--- Sales.API/Controllers/AccountsController.cs
+++ Sales.API/Controllers/AccountsController.cs
@@ -139,6 +139,37 @@ namespace Sales.API.Controllers
             return BadRequest(result.Errors.FirstOrDefault());
         }
 
+        [HttpPost("ResedToken")]
+        public async Task<ActionResult> ResedToken([FromBody] EmailDTO model)
+        {
+            User user = await _userHelper.GetUserAsync(model.Email);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            //TODO: Improve code 
+            var myToken = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+            var tokenLink = Url.Action("ConfirmEmail", "accounts", new
+            {
+                userid = user.Id,
+                token = myToken
+            }, HttpContext.Request.Scheme, _configuration["UrlWEB"]);
+
+            var response = _mailHelper.SendMail(user.FullName, user.Email!,
+                $"Saless- Confirmación de cuenta",
+                $"<h1>Sales - Confirmación de cuenta</h1>" +
+                $"<p>Para habilitar el usuario, por favor hacer clic 'Confirmar Email':</p>" +
+                $"<b><a href ={tokenLink}>Confirmar Email</a></b>");
+
+            if (response.IsSuccess)
+            {
+                return NoContent();
+            }
+
+            return BadRequest(response.Message);
+        }
+
         [HttpGet("ConfirmEmail")]
         public async Task<ActionResult> ConfirmEmailAsync(string userId, string token)
         {
