ccaa961252559fa8580e98e40d2831f89a7db05b
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-03-23 21:07:12 -0500

    Product creation

---
+++ Sales.WEB/Helpers/MultipleSelectorModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Sales.WEB.Helpers
+{
+    public class MultipleSelectorModel
+    {
+        public MultipleSelectorModel(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
