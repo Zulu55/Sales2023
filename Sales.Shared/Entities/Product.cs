d716df25bd62ff4c48ebee84004de0a20cfe1a39
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-03-23 20:38:47 -0500

    Add product tables & improving the seed DB

---
+++ Sales.Shared/Entities/Product.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sales.Shared.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Descripción")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Description { get; set; } = null!;
+
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal Price { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        [Display(Name = "Inventario")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public float Stock { get; set; }
+
+        public ICollection<ProductCategory>? ProductCategories { get; set; }
+
+        [Display(Name = "Categorías")]
+        public int ProductCategoriesNumber => ProductCategories == null ? 0 : ProductCategories.Count;
+
+        public ICollection<ProductImage>? ProductImages { get; set; }
+
+        [Display(Name = "Imágenes")]
+        public int ProductImagesNumber => ProductImages == null ? 0 : ProductImages.Count;
+
+        [Display(Name = "Imagén")]
+        public string MainImage => ProductImages == null ? string.Empty : ProductImages.FirstOrDefault()!.Image;
+
+    }
+
+}
---
+++ Sales.Shared/Entities/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; } = null!;
+
+        public int ProductId { get; set; }
+
+        [Display(Name = "Imagen")]
+        public string Image { get; set; } = null!;
+    }
+}
