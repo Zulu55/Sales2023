17cf4f78aa089530d7c31a49bd2a204336e9309d
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-03-30 20:52:04 -0500

    orders index

--- Sales.API/Controllers/SalesController.cs
+++ Sales.API/Controllers/SalesController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
 using Sales.API.Helpers;
 using Sales.Shared.DTOs;
+using Sales.Shared.Enums;
 
 namespace Sales.API.Controllers
 {
@@ -12,10 +15,14 @@ namespace Sales.API.Controllers
     public class SalesController : ControllerBase
     {
         private readonly IOrdersHelper _ordersHelper;
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
 
-        public SalesController(IOrdersHelper ordersHelper)
+        public SalesController(IOrdersHelper ordersHelper, DataContext context, IUserHelper userHelper)
         {
             _ordersHelper = ordersHelper;
+            _context = context;
+            _userHelper = userHelper;
         }
 
         [HttpPost]
@@ -28,6 +35,56 @@ namespace Sales.API.Controllers
             }
 
             return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get([FromQuery] PaginationDTO pagination)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == User.Identity!.Name);
+            if (user == null)
+            {
+                return BadRequest("User not valid.");
+            }
+
+            var queryable = _context.Sales
+                .Include(s => s.User!)
+                .Include(s => s.SaleDetails!)
+                .ThenInclude(sd => sd.Product)
+                .AsQueryable();
+
+            var isAdmin = await _userHelper.IsUserInRoleAsync(user, UserType.Admin.ToString());
+            if (!isAdmin)
+            {
+                queryable = queryable.Where(s => s.User!.Email == User.Identity!.Name);
+            }
+
+            return Ok(await queryable
+                .OrderByDescending(x => x.Date)
+                .Paginate(pagination)
+                .ToListAsync());
+        }
+
+        [HttpGet("totalPages")]
+        public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == User.Identity!.Name);
+            if (user == null)
+            {
+                return BadRequest("User not valid.");
+            }
+
+            var queryable = _context.Sales
+                .AsQueryable();
+
+            var isAdmin = await _userHelper.IsUserInRoleAsync(user, UserType.Admin.ToString());
+            if (!isAdmin)
+            {
+                queryable = queryable.Where(s => s.User!.Email == User.Identity!.Name);
+            }
+
+            double count = await queryable.CountAsync();
+            double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
+            return Ok(totalPages);
         }
     }
 }
